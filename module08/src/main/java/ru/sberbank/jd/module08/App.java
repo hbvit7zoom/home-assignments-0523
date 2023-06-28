4de76b79f75989c575469c339ca11227d9dade08
Nikita Kozlov <nigkozlov@sberbank.ru>
2023-06-28 23:34:23 +0300

    webinar08 added

---
+++ module08/src/main/java/ru/sberbank/jd/module08/App.java
@@ -0,0 +1,13 @@
+package ru.sberbank.jd.module08;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
